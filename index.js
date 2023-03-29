248dcaf2ec57292477a73dc8fd9f67a29c9498fc
dharanicb <dharanicb86391@gmail.com>
2023-03-29 06:00:22 +0000

    solution

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-color">
+    <h1 className="tittle">Super Over Leangue</h1>
+    <div className="content">
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="image"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
